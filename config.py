dc8ab0deeb57744f0e40f8d2c9b01872e2df9c1f
Gegegedo <wlm1994@live.com>
2020-02-11 00:58:00 +0800

    完善生成数据集代码

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class DatasetConfig:
-    remote_root='/dataset'
+    remote_root='/home/zhou/wlm/RSF/dataset'
     img_names=['GF2_PMS2_E117.4_N39.1_20170510_L1A0002351826','GF2_PMS1_E117.6_N39.0_20171108_L1A0002748722']
                 # 'GF2_PMS1_E117.6_N39.2_20171108_L1A0002748717',\]
     train_scale,validate_scale,test_scale=0.8,0.1,0.1
--- img2record.py
+++ img2record.py
@@ -4,6 +4,11 @@ from config import DatasetConfig as DC
 import tensorflow as tf
 import glob
 import random
+import cv2
+import os
+def down_sample(img,scale=4):
+    return cv2.resize(img,dsize=(0,0),fx=1/scale,fy=1/scale,\
+        interpolation=cv2.INTER_CUBIC)
 def read_tif(name):
     ms_path,pan_path=glob.glob(DC.remote_root+'/'+name+'-MSS?.tiff')[0],\
         glob.glob(DC.remote_root+'/'+name+'-PAN?.tiff')[0]
@@ -15,12 +20,12 @@ def read_tif(name):
 def read_patch(idx,ms,pan,col_patch_num,row_patch_num):
     band=ms.RasterCount
     row,col=idx//col_patch_num,idx%col_patch_num
-    ms_patch,pan_patch=np.zeros((DC.MS_crop_size,DC.MS_crop_size,4),np.float32),\
-        np.zeros((DC.MS_crop_size*4,DC.MS_crop_size*4,1),np.float32)
+    ms_patch,pan_patch=np.zeros((DC.MS_crop_size,DC.MS_crop_size,4),np.uint16),\
+        np.zeros((DC.MS_crop_size*4,DC.MS_crop_size*4,1),np.uint16)
     for b in range(1,band+1):
         ms_patch[:,:,b-1]=ms.GetRasterBand(b).ReadAsArray(col,row,DC.MS_crop_size,DC.MS_crop_size)
     pan_patch[:,:,0]=pan.GetRasterBand(1).ReadAsArray(col*4,row*4,DC.MS_crop_size*4,DC.MS_crop_size*4)
-    return ms_patch/DC.Max_Pixel,pan_patch/DC.Max_Pixel
+    return ms_patch,pan_patch
 def sampler(col_patch,row_patch):
     num=col_patch*row_patch
     all_idx=set(range(num))
@@ -30,19 +35,22 @@ def sampler(col_patch,row_patch):
     test_idx=all_idx-set(val_idx)
     return train_idx,val_idx,test_idx
 def write_patch(ms,pan,writer):
-    ms_feature,pan_feature=tf.train.Feature(bytes_list=tf.train.BytesList(value=[ms.tostring()])),\
-        tf.train.Feature(bytes_list=tf.train.BytesList(value=[pan.tostring()]))
-    features=tf.train.Features(feature={'ms':ms_feature,'pan':pan_feature})
+    input_ms,input_pan=down_sample(ms),down_sample(pan)
+    ms_feature,pan_feature,fusion=\
+        tf.train.Feature(bytes_list=tf.train.BytesList(value=[input_ms.tostring()])),\
+        tf.train.Feature(bytes_list=tf.train.BytesList(value=[input_pan.tostring()])),\
+        tf.train.Feature(bytes_list=tf.train.BytesList(value=[ms.tostring()]))
+    features=tf.train.Features(feature={'ms':ms_feature,'pan':pan_feature,\
+                                'fusion':fusion})
     example=tf.train.Example(features=features)
     writer.write(example.SerializeToString())
 if __name__ == "__main__":
-    for img_idx,img in enumerate(DC.img_names[1:]):
+    for img_idx,img in enumerate(DC.img_names):
         img_idx+=1
-        record_names=['train%d_%d'%(DC.MS_crop_size,img_idx),'val%d_%d'%(DC.MS_crop_size,img_idx),\
-            'test%d_%d'%(DC.MS_crop_size,img_idx)]
         ms,pan,col_patch_num,row_patch_num=read_tif(img)
-        sample_idx=sampler(col_patch_num,row_patch_num )
-        for i,record_name in enumerate(record_names):
+        sample_idx=sampler(col_patch_num,row_patch_num)
+        for i,catagory in enumerate(['train','val','test']):
+            record_name=os.path.join(os.getcwd(),'dataset/%s/%d.tfrecord'%(catagory,img_idx))
             with tf.io.TFRecordWriter(record_name) as writer:
                 for idx in sample_idx[i]:
                     ms_patch,pan_patch=read_patch(idx,ms,pan,col_patch_num,row_patch_num)
